1ecaf504dd6ee85fb42be6432d93446ff78a3a8d
sushankg18 <sushankg4@gmail.com>
2024-08-05 20:45:37 +0530

    added

--- frontend/src/hooks/getOtherUsers.jsx
+++ frontend/src/hooks/getOtherUsers.jsx
@@ -4,9 +4,7 @@ import { useDispatch } from 'react-redux'
 import { setOtherUsers } from '../redux/userSlice'
 
 const GetOtherUsers = () => {
-
   const dispatch = useDispatch()
-
   useEffect(() => {
     const fetchOtherUsers = async () => {
       try {
@@ -15,7 +13,6 @@ const GetOtherUsers = () => {
         console.log("Other users : ",response.data.otherUsers)
         dispatch(setOtherUsers(response.data.otherUsers))
       } catch (error) {
-
         console.log("Error while fetching otherUsers : ", error)
       }
     };
